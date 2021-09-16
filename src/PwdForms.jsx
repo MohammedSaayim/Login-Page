e20f38de2895f6dca6e6d2b3d7e4e27980093a70
Mohammed Saayim <mdsaayim@gmail.com>
2021-09-16 07:17:22 +0530

    initial commit

--- src/PwdForms.jsx
+++ src/PwdForms.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import SubmitBtn from "./SubmitBtn";
 
+// npm i --save @fortawesome/fontawesome-svg-core
+// npm i --save @fortawesome/free-solid-svg-icons
+// npm i --save @fortawesome/react-fontawesome
+
 export default function PwdForms() {
   const [pwd, setPwd] = useState("");
   const [rePwd, setRePwd] = useState("");
+  const [inputType1, setInputType1] = useState("password");
+  const [inputType2, setInputType2] = useState("password");
 
   const validPwd = new RegExp("[0-9]");
   let matchColor, alphaColor;
@@ -75,20 +81,54 @@ export default function PwdForms() {
     setRePwd(event.target.value);
   }
 
+  function toggleVisibility1() {
+    if (inputType1 === "password") {
+      setInputType1("text");
+    } else {
+      setInputType1("password");
+    }
+  }
+
+  function toggleVisibility2() {
+    if (inputType2 === "password") {
+      setInputType2("text");
+    } else {
+      setInputType2("password");
+    }
+  }
+
+  function ShowPwd1() {
+    return (
+      <span>
+        <button onClick={toggleVisibility1}>Show password</button>
+      </span>
+    );
+  }
+
+  function ShowPwd2() {
+    return (
+      <span>
+        <button onClick={toggleVisibility2}>Show password</button>
+      </span>
+    );
+  }
+
   return (
     <div>
       <div>
         <label>
           Enter password:
-          <input onChange={onPwdChange} type="password"></input>
+          <input onChange={onPwdChange} type={inputType1}></input>
         </label>
+        <ShowPwd1 />
       </div>
 
       <div>
         <label>
           Re-enter password:
-          <input onChange={onRePwdChange} type="password"></input>
+          <input onChange={onRePwdChange} type={inputType2}></input>
         </label>
+        <ShowPwd2 />
       </div>
 
       <PwdMatch />
